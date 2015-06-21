31dfbda217cb237261c03d00dc83be138c744e57
edpowley <edpowley@gmail.com>
2015-06-21 18:15:59 +0100

    Basic boat movement

---
+++ Assets/Scripts/Boat.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Boat : MonoBehaviour
+{
+    private List<Vector3> m_course = new List<Vector3>();
+    private bool m_courseBeingDrawn = false;
+
+    public float MovementSpeed = 10;
+    public float RotationSpeed = 90;
+
+    public MeshFilter CourseLineMeshFilter;
+    private Mesh m_courseLineMesh;
+
+    private LineMesher m_courseLineMesher = new LineMesher();
+
+    // Use this for initialization
+    void Start()
+    {
+        m_courseLineMesh = new Mesh();
+        CourseLineMeshFilter.mesh = m_courseLineMesh;
+
+        StartCoroutine(handleMouse());
+    }
+
+    private IEnumerator handleMouse()
+    {
+        while (true)
+        {
+            while (!Input.GetMouseButtonDown(0))
+            {
+                yield return null;
+            }
+
+            // Mouse button is down
+            m_course.Clear();
+            m_courseBeingDrawn = true;
+
+            while (Input.GetMouseButton(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Vector3 yIntercept = ray.GetPoint(-ray.origin.y / ray.direction.y);
+                
+                if (!Mathf.Approximately(yIntercept.y, 0))
+                    Debug.LogErrorFormat("yIntercept.y == {0} != 0", yIntercept.y);
+                
+                yIntercept.y = 0;
+                
+                Debug.LogFormat("yIntercept: {0}", yIntercept);
+
+                if (m_course.Count == 0 || Vector3.Distance(yIntercept, m_course[m_course.Count - 1]) > 0.5f)
+                {
+                    m_course.Add(yIntercept);
+                }
+
+                yield return null;
+            }
+
+            m_courseBeingDrawn = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_courseBeingDrawn)
+        {
+            float movementStepSize = MovementSpeed * Time.deltaTime;
+
+            while (movementStepSize > 0 && m_course.Count > 0)
+            {
+                Vector3 target = m_course[0];
+                Vector3 delta = target - transform.position;
+
+                float deltaSize = delta.magnitude;
+                if (deltaSize < movementStepSize)
+                {
+                    transform.position = target;
+                    movementStepSize -= deltaSize;
+                    m_course.RemoveAt(0);
+                }
+                else
+                {
+                    transform.position += delta / deltaSize * movementStepSize;
+                    movementStepSize = 0;
+                }
+
+                Quaternion targetRotation = Quaternion.FromToRotation(Vector3.right, delta);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+            }
+        }
+
+        if (m_course.Count > 0)
+        {
+            m_courseLineMesher.Clear();
+            List<Vector2> course = new List<Vector2>();
+            course.Add(new Vector2(transform.position.x, transform.position.z));
+            foreach(Vector3 pos in m_course)
+            {
+                course.Add(new Vector2(pos.x, pos.z));
+            }
+
+            m_courseLineMesher.c_radius = 0.25f;
+            m_courseLineMesher.AddLineStripAsSegmentsByAngle(course);
+            m_courseLineMesher.PopulateMesh(m_courseLineMesh);
+        }
+        else
+        {
+            m_courseLineMesh.Clear();
+        }
+    }
+}
--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -1,36 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Camera control script for tracking and zooming on an object.
+/// </summary>
+[ExecuteInEditMode]
 public class CameraZoom : MonoBehaviour
 {
     private Camera m_camera;
     private bool m_isZoomInProgress;
     private Vector3 m_lastDragPos;
-    public float m_zoom = 0.0f;
-    public float m_zoomDragScale = 0.001f;
-    public Vector3 m_offset = new Vector3(0, 2, -4);
 
-    public Transform m_target;
+    /// <summary>
+    /// Zoom level: 0 = max zoom in, 1 = max zoom out
+    /// </summary>
+    [Range(0, 2)]
+    public float Zoom = 0.0f;
+
+    /// <summary>
+    /// Mouse zoom sensitivity, in zoom units per pixel moved
+    /// </summary>
+    public float ZoomDragScale = 0.001f;
+
+    public float ZoomWheelScale = 0.001f;
+
+    /// <summary>
+    /// Offset vector of the camera (at zoom 0) from the target
+    /// </summary>
+    public Vector3 Offset = new Vector3(0, 2, -4);
+
+    public float MidZoomY = 500;
+    public Vector2 MaxZoomHalfArea = new Vector2(512, 512);
+
+    /// <summary>
+    /// The target
+    /// </summary>
+    public Transform Target;
 
     void Start()
     {
         m_camera = GetComponent<Camera>();
         m_isZoomInProgress = false;
+
     }
 	
     void Update()
     {
         if (m_isZoomInProgress)
         {
-            if (Input.GetMouseButtonUp(1))
+            if (Input.GetMouseButtonUp(1)) // 1 = right mouse button
             {
+                // Mouse button released -- stop zoom
                 m_isZoomInProgress = false;
             }
             else
             {
+                // Mouse button still pressed update zoom
                 Vector3 delta = m_lastDragPos - Input.mousePosition;
-                m_zoom += delta.y * m_zoomDragScale;
-                m_zoom = Mathf.Clamp01(m_zoom);
+                Zoom += delta.y * ZoomDragScale;
                 m_lastDragPos = Input.mousePosition;
             }
         }
@@ -38,13 +65,38 @@ public class CameraZoom : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(1))
             {
+                // Mouse button pressed -- start zoom
                 m_isZoomInProgress = true;
                 m_lastDragPos = Input.mousePosition;
             }
         }
 
-        Vector3 targetPos = m_target.position;
-        transform.position = new Vector3(targetPos.x, m_zoom * 500, targetPos.z) + m_offset;
-        transform.LookAt(targetPos, Vector3.up);
+        Zoom += Input.mouseScrollDelta.y * ZoomWheelScale;
+        Zoom = Mathf.Clamp(Zoom, 0.0f, 2.0f);
+
+        // Get target position
+        Vector3 targetPos;
+        if (Target != null)
+            targetPos = Target.position;
+        else 
+            targetPos = Vector3.zero;
+
+        // Update camera position and rotation
+        Vector3 lowZoomPos = new Vector3(targetPos.x, Mathf.Clamp01(Zoom) * MidZoomY, targetPos.z) + Offset;
+
+        if (Zoom <= 1.0f)
+        {
+            transform.position = lowZoomPos;
+            transform.LookAt(targetPos, Vector3.up);
+        }
+        else
+        {
+            float maxZoomY = MaxZoomHalfArea.y / Mathf.Tan(0.5f * m_camera.fieldOfView * Mathf.Deg2Rad);
+            Vector3 maxZoomPos = new Vector3(0, maxZoomY, 0);
+
+            transform.position = Vector3.Lerp(lowZoomPos, maxZoomPos, Zoom - 1);
+
+            transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
     }
 }
