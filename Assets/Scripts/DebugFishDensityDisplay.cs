35f4c5b77cf2985acec0305398709a5266a78f4b
edpowley <edpowley@gmail.com>
2015-09-10 20:33:36 +0100

    Added fish density display for debugging

---
+++ Assets/Scripts/DebugFishDensityDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugFishDensityDisplay : MonoBehaviour
+{
+    private Renderer m_renderer;
+    public Renderer m_waterRenderer;
+
+    void Start()
+    {
+        m_renderer = GetComponent<Renderer>();
+    }
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            m_renderer.enabled = !m_renderer.enabled;
+            m_waterRenderer.enabled = !m_renderer.enabled;
+        }
+	}
+}
