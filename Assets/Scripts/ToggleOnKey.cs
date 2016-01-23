75d3a9792e15a41b78c9a5110f41717b970bb8b1
edpowley <edpowley@gmail.com>
2016-01-23 17:24:28 +0000

    Show/hide perf graph on F2 key

---
+++ Assets/Scripts/ToggleOnKey.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleOnKey : MonoBehaviour
+{
+    public KeyCode m_key;
+    private bool m_enabled;
+    public bool m_startEnabled;
+
+    void Start()
+    {
+        setEnabled(m_startEnabled);
+    }
+
+    private void setEnabled(bool enabled)
+    {
+        m_enabled = enabled;
+
+        for(int i=0;i< transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            child.gameObject.SetActive(enabled);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(m_key))
+            setEnabled(!m_enabled);
+    }
+}
