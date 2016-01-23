7c4e60137e4c7ad9f6a34515afd3e98c26dfc845
edpowley <edpowley@gmail.com>
2016-01-23 15:26:41 +0000

    Added perf graph

---
+++ Assets/Scripts/FpsCounter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FpsCounter : MonoBehaviour
+{
+	private float m_elapsedTime;
+	private int m_elapsedFrames;
+	private GUIText m_oldText; // pre Unity 4.6
+	private UnityEngine.UI.Text m_newText; // post Unity 4.6
+
+	void Start()
+	{
+		m_oldText = GetComponent<GUIText>();
+		m_newText = GetComponent<UnityEngine.UI.Text>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		m_elapsedFrames++;
+		m_elapsedTime += Time.unscaledDeltaTime;
+
+		if (m_elapsedTime >= 1.0f)
+		{
+			float fps = m_elapsedFrames / m_elapsedTime;
+			m_elapsedFrames = 0;
+			m_elapsedTime = 0;
+
+			string text = string.Format("{0:0.00} FPS", fps);
+			if (m_oldText != null) m_oldText.text = text;
+			if (m_newText != null) m_newText.text = text;
+		}
+	}
+}
---
+++ Assets/Scripts/PerfGraphForCanvas.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PerfGraphForCanvas : MonoBehaviour
+{
+	public float m_sampleTime = 5.0f;
+	public float m_yUnit = 1.0f / 60.0f;
+	public bool m_isActive = false;
+
+	private struct DataPoint { public float x, y; }
+	private List<DataPoint> m_data = new List<DataPoint>();
+
+	private RectTransform m_rectTransform;
+	private CanvasRenderer m_renderer;
+	public Material m_material;
+
+	void Start()
+	{
+		m_rectTransform = GetComponent<RectTransform>();
+		m_renderer = GetComponent<CanvasRenderer>();
+		m_renderer.SetMaterial(m_material, null);
+
+		StartCoroutine(updateLoop());
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		m_data.Add(new DataPoint { x = Time.unscaledTime, y = Time.unscaledDeltaTime });
+
+		if (Input.GetKeyDown(KeyCode.F2))
+		{
+			m_isActive = !m_isActive;
+
+			if (m_isActive)
+				updateMesh();
+			else
+				m_renderer.SetVertices(new List<UIVertex>());
+		}
+	}
+
+	private IEnumerator updateLoop()
+	{
+		float lastUpdateTime = Time.unscaledTime;
+		for (; ; )
+		{
+			while (Time.unscaledTime < lastUpdateTime + 0.5f)
+				yield return null;
+
+			lastUpdateTime = Time.unscaledTime;
+
+			removeOldPoints();
+
+			if (m_isActive)
+				updateMesh();
+		}
+	}
+
+	private void removeOldPoints()
+	{
+		int numToRemove;
+		float threshold = Time.unscaledTime - m_sampleTime;
+		for (numToRemove = 0; numToRemove < m_data.Count; numToRemove++)
+		{
+			if (m_data[numToRemove].x >= threshold)
+				break;
+		}
+
+		m_data.RemoveRange(0, numToRemove);
+	}
+
+	private void updateMesh()
+	{
+		Rect targetRect;
+		if (m_rectTransform != null)
+			targetRect = m_rectTransform.rect;
+		else
+			targetRect = new Rect(0, 0, 1, 1);
+
+		float xScale = targetRect.width / m_sampleTime;
+		float xOff = Time.unscaledTime - m_sampleTime;
+		//float yScale = 1.0f / m_data.Max();
+		float yScale = targetRect.height / m_yUnit * 0.25f;
+
+		List<UIVertex> vertices = new List<UIVertex>();
+
+		for (int i = 0; i < m_data.Count; i++)
+		{
+			DataPoint p = m_data[i];
+			float x1 = (p.x - p.y - xOff) * xScale + targetRect.xMin;
+			float x2 = (p.x - xOff) * xScale + targetRect.xMin;
+			float y1 = targetRect.yMin;
+			float y2 = p.y * yScale + targetRect.yMin;
+			float v = p.y / m_yUnit * 0.5f;
+
+			vertices.Add(new UIVertex { position = new Vector3(x1, y1, 0), uv0 = new Vector2(0, 0) });
+			vertices.Add(new UIVertex { position = new Vector3(x2, y1, 0), uv0 = new Vector2(0, 0) });
+			vertices.Add(new UIVertex { position = new Vector3(x2, y2, 0), uv0 = new Vector2(0, v) });
+			vertices.Add(new UIVertex { position = new Vector3(x1, y2, 0), uv0 = new Vector2(0, v) });
+		}
+
+		m_renderer.SetVertices(vertices);
+	}
+}
